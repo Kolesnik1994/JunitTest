78492bc6f080da2e77e823a2665d17a41cbfef3f
Java <Java@DESKTOP-DGJGE67>
2023-02-08 22:24:23 +0200

    Create junit test class DemoUtilTest.java for DemoUtil.class

---
+++ JunitTests/src/test/java/test/DemoUtilTest.java
@@ -0,0 +1,151 @@
+package test;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ * Java class that testing method consist in DemoUtil.java class
+ * @author JCODER
+ * 
+ */
+
+@DisplayNameGeneration (DisplayNameGenerator.Simple.class)
+public class DemoUtilTest {
+	
+	DemoUtils demoUtils;
+	
+	 @BeforeEach
+	 void setupBeforeEach() {
+		 demoUtils = new DemoUtils();
+		 System.out.println ("@BeforeEach executes before the execution of each test method");
+	 }
+	 
+	 @AfterEach
+	 void tearDownAfterEach () {
+		 System.out.println ("Running @AfterEach");
+	 }
+	 
+	 /*
+	 @BeforeAll
+	 static void setupBeforerAllClass () {
+		 System.out.println ("@BeforeAll");
+	 }
+	 
+	 @AfterAll
+	 static void setupAfterAllClass() {
+	 System.out.println ("@AfterAll");
+	 }
+     */
+	 
+	 
+	 /* assertEquals - > testing that 6 equals sum of number in method add
+	  * assertNotEquals -> testing that 6 not equals sum of number in method add 
+	  */
+	@Test
+	@DisplayName ("Equals and not Equals")
+	void testEqualsAndNotEquals () {
+		
+		System.out.println ("Running test: testEqualsAndNotEquals");
+		
+		Assertions.assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
+		Assertions.assertNotEquals (6, demoUtils.add(2, 5), "2+5 must be 7");
+	}
+		
+	 /* assertNull - > testing that str1 should be null
+	  * assertNotEquals -> testing str2 should not be null
+	  */
+	@Test
+	@DisplayName ("Null and not Null")
+	void testNullAndNotNull() {
+		 
+		System.out.println ("Running test: testNullAndNotNull");
+		
+		String str1	= null;
+		String str2 = "luvCode";
+		
+		Assertions.assertNull (demoUtils.checkNull(str1), "Object should be null");
+		Assertions.assertNotNull (demoUtils.checkNull(str2), "Object shoul be not null");
+		
+	}
+	
+	 /* assertTrue - > testing that int a will be greater than the number g
+	  * assertNotEquals -> testing that int g will be less than the number g
+	  */
+	@Test
+	@DisplayName ("True and False")
+	void testSameAndNotSame() {
+		
+		int g = 2;
+		int a = 5;
+		
+		Assertions.assertTrue(demoUtils.isGreater(a, g), "This should return true");
+		Assertions.assertFalse(demoUtils.isGreater(g, a), "This should retrun false");
+	}
+	
+	 /* assertArrayEquals - > testing that String[] stringArray equals String[] in method getFirstThreeLettersOfAlphabet
+	  */
+	@DisplayName ("Arrays Equals")
+	@Test
+	void testArrayEquals() {
+	String[] stringArray = {"A", "B", "C"};	
+	
+	Assertions.assertArrayEquals (stringArray, demoUtils.getFirstThreeLettersOfAlphabet(), "Array should be same");
+	}
+	
+	 /* assertIterableEquals - > testing that String[] theList iterables are deeply equals String[] in method getAcademyInList
+	  */
+	@DisplayName ("Iterable Equals")
+	@Test
+	void testIterableEquals() {
+		List <String> theList= List.of("luv", "2", "code");
+		
+		Assertions.assertIterableEquals (theList, demoUtils.getAcademyInList(), "Expected list should be the same");
+	}
+	
+	 /* Lines Match - > testing that String[] theList matches String[] in method getAcademyInList
+	  */
+	@DisplayName ("Lines Match")
+	@Test
+	void testLinesMatch() {
+		List <String> theList= List.of("luv", "2", "code");
+		
+		Assertions.assertLinesMatch (theList, demoUtils.getAcademyInList(), "Lines should match");
+	}
+	
+	 /* assertThrows - > execute Exception if number in method throwException less than 0 
+	  * assertDoesNotThrow -> does not execute Exception if number in method throwException biggest than 0
+	  */
+	@DisplayName ("Throw and Does Not Throw Exception")
+	@Test
+	void testThrowException () {
+		
+		Assertions.assertThrows(Exception.class, ()-> {demoUtils.throwException(-1); }, "Should throw Exception");
+		
+		Assertions.assertDoesNotThrow (()-> {demoUtils.throwException (5);}, "Does Not Throw");
+	}
+	
+	 /* assertTimeoutPreemptively - >  testing that method checkTimeout executed duration 3s
+	  */
+	@DisplayName ("Timeout")
+	@Test
+	void testTimeout () {
+		Assertions.assertTimeoutPreemptively(Duration.ofSeconds(3), ()-> {demoUtils.checkTimeout();}, "Methoud should execute in 3 second");
+	}
+	
+}
+		
+	
+	
+
+
